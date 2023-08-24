8be8c59faca1614ad58d1fefd94e0dd3a69533f5
emireyes <141955808+emireyes@users.noreply.github.com>
2023-08-24 12:34:18 -0600

    Add files via upload

---
+++ asignacion2.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+	
+	int a=5;
+	int b=6;
+	float potenciavalores=pow(a,b);
+	
+	printf("el valor la potencia a^b es %f", potenciavalores);
+	printf("/n");
+	printf("el valor de a-b es %d", a-b);
+	
+
+	
+	printf ("\a");
+	return 0;
+	
+	
+	
+	
+}
+
+	/*
+	float a;
+	printf("introduzca un valor entero\n");
+	scanf("%f", &a);//*/ 	
